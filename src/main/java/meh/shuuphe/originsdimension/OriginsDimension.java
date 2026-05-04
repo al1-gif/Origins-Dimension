ec2f62ef70128561299d90eb2d19b6bf4712287a
al1-gif <dreaddevil51@gmail.com>
2026-05-04 16:29:37 +0545

    initial commit

--- src/main/java/meh/shuuphe/originsdimension/OriginsDimension.java
+++ src/main/java/meh/shuuphe/originsdimension/OriginsDimension.java
@@ -11,6 +11,6 @@ public class OriginsDimension implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Origins Dimension starting!!!...");
 	}
 }
