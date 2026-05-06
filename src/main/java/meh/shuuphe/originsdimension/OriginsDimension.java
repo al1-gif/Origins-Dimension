ec4e3c341f6170332f354330adf4844863b64001
al1-gif <dreaddevil51@gmail.com>
2026-05-06 13:38:08 +0545

    initial commit

--- src/main/java/meh/shuuphe/originsdimension/OriginsDimension.java
+++ src/main/java/meh/shuuphe/originsdimension/OriginsDimension.java
@@ -2,6 +2,9 @@ package meh.shuuphe.originsdimension;
 
 import net.fabricmc.api.ModInitializer;
 
+import meh.shuuphe.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +15,14 @@ public class OriginsDimension implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Origins Dimension starting!!!...");
+		ModDimensions.register();
+		ModBiomes.register();
+
+		CustomPortalBuilder.beginPortal()
+				.frameBlock(Blocks.CRYING_OBSIDIAN)
+				.lightWithWater()
+				.destDimID(Identifier.of("originsdimension", "origin"))
+				.tintColor(255, 200, 0)
+				.registerPortal();
 	}
 }
